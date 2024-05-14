bec96a73c965b2a9d9954a5e8428f1cc61abf2d7
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-14 17:49:35 +0000

    Extract DB types into schema file

---
+++ src/database/schema.types.ts
@@ -0,0 +1,34 @@
+import { Document } from 'mongoose';
+
+export interface User extends Document {
+  readonly name: string;
+  readonly email: string;
+}
+
+export interface Country extends Document {
+  readonly name: string;
+  readonly currency: string;
+  readonly tax_brackets: {
+    bracket: string;
+    rate: number;
+    limit: number;
+  }[];
+}
+
+export interface Deduction extends Document {
+  readonly user_id: string;
+  readonly description: string;
+  readonly income: number;
+  readonly year: number;
+  readonly country_id: string;
+  readonly tax: number;
+}
+
+export interface Summary extends Document {
+  readonly user_id: string;
+  readonly year: number;
+  readonly country_id: string;
+  readonly total_taxed_income: number;
+  readonly total_deducted_tax: number;
+  readonly current_tax_index: number;
+}
