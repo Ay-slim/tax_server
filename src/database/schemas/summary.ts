6970ebdbd0edbee7741769af75da06ef9973dc3f
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-14 08:58:30 +0000

    Summary model update

--- src/database/schemas/summary.ts
+++ src/database/schemas/summary.ts
@@ -5,8 +5,9 @@ export const Summary = new mongoose.Schema(
     user_id: mongoose.Types.ObjectId,
     year: Number,
     country_id: mongoose.Types.ObjectId,
-    income: Number,
-    tax: Number,
+    total_taxed_income: Number,
+    total_deducted_tax: Number,
+    current_tax_index: Number,
   },
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
 );
