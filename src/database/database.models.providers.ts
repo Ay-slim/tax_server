e1e64472bcd6fb6fdbae96e647036172f57fb0d1
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-14 18:12:09 +0000

    Remove unnecessary model exports

--- src/database/database.models.providers.ts
+++ src/database/database.models.providers.ts
@@ -2,38 +2,37 @@ import { Connection } from 'mongoose';
 import { Country } from './schemas/country';
 import { Summary } from './schemas/summary';
 import { User } from './schemas/user';
+import { Deduction } from './schemas/deduction';
 
-export const CountryProviders = [
-  {
-    provide: 'COUNTRY_MODEL',
-    useFactory: (connection: Connection) =>
-      connection.model('Country', Country),
-    inject: ['DATABASE_CONNECTION'],
-  },
-];
+const CountryModel = {
+  provide: 'COUNTRY_MODEL',
+  useFactory: (connection: Connection) => connection.model('Country', Country),
+  inject: ['DATABASE_CONNECTION'],
+};
 
-export const DeductionProviders = [
-  {
-    provide: 'DEDUCTION_MODEL',
-    useFactory: (connection: Connection) =>
-      connection.model('Deduction', Country),
-    inject: ['DATABASE_CONNECTION'],
-  },
-];
+const DeductionModel = {
+  provide: 'DEDUCTION_MODEL',
+  useFactory: (connection: Connection) =>
+    connection.model('Deduction', Deduction),
+  inject: ['DATABASE_CONNECTION'],
+};
 
-export const SummaryProviders = [
-  {
-    provide: 'SUMMARY_MODEL',
-    useFactory: (connection: Connection) =>
-      connection.model('Summary', Summary),
-    inject: ['DATABASE_CONNECTION'],
-  },
-];
+const SummaryModel = {
+  provide: 'SUMMARY_MODEL',
+  useFactory: (connection: Connection) => connection.model('Summary', Summary),
+  inject: ['DATABASE_CONNECTION'],
+};
 
-export const UserProviders = [
-  {
-    provide: 'USER_MODEL',
-    useFactory: (connection: Connection) => connection.model('User', User),
-    inject: ['DATABASE_CONNECTION'],
-  },
-];
+const UserModel = {
+  provide: 'USER_MODEL',
+  useFactory: (connection: Connection) => connection.model('User', User),
+  inject: ['DATABASE_CONNECTION'],
+};
+
+export const CountryProviders = [CountryModel];
+
+export const DeductionProviders = [DeductionModel, SummaryModel, CountryModel];
+
+export const UserProviders = [UserModel, SummaryModel];
+
+export const SummaryProviders = [SummaryModel];
