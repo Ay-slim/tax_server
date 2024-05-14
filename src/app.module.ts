486ea3685127dd9db57980504e47bcd2b9eeda37
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-14 08:55:46 +0000

    Include country module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/module';
+import { CountryModule } from './country/module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CountryModule],
 })
 export class AppModule {}
