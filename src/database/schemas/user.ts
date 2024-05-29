f9f94691364fa30b6f4b7a34496a974030550631
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-29 11:14:39 +0000

    Include password field in user model

--- src/database/schemas/user.ts
+++ src/database/schemas/user.ts
@@ -4,6 +4,10 @@ export const User = new mongoose.Schema(
   {
     name: String,
     email: String,
+    password: {
+      type: String,
+      required: true,
+    },
   },
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
 );
