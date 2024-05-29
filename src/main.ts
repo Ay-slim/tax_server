dd7e5b7bb7eb157ebec591aaea30aa8d61d2d178
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-29 03:50:17 +0000

    Cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: false,
+  });
   await app.listen(3002);
 }
 bootstrap();
