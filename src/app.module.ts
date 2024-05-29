1fb77bc3a2c9e695d949498c56a91a67718ccad4
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-29 03:50:01 +0000

    Deduction and Summary modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/module';
 import { CountryModule } from './country/module';
+import { DeductionModule } from './tax_deduction/module';
+import { SummaryModule } from './summary/module';
 
 @Module({
-  imports: [UserModule, CountryModule],
+  imports: [UserModule, CountryModule, DeductionModule, SummaryModule],
 })
 export class AppModule {}
