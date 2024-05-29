50f2aa5e9b1d4267c08530bd458f1dddf33622b5
Ay-slim <ayooluwaadedipe@gmail.com>
2024-05-29 03:51:06 +0000

    Add current tax bracket to summary

--- src/database/schemas/summary.ts
+++ src/database/schemas/summary.ts
@@ -8,6 +8,7 @@ export const Summary = new mongoose.Schema(
     total_taxed_income: Number,
     total_deducted_tax: Number,
     current_tax_index: Number,
+    current_tax_bracket: String,
   },
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
 );
