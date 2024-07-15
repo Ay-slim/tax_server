1fbe460f2faabed028cb22e2635ffd75160cc46d
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-15 06:15:33 +0000

    Use env variable for mongo uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { UserModule } from './user/module';
 import { CountryModule } from './country/module';
 import { DeductionModule } from './tax_deduction/module';
 import { SummaryModule } from './summary/module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, CountryModule, DeductionModule, SummaryModule],
+  imports: [
+    UserModule,
+    CountryModule,
+    DeductionModule,
+    SummaryModule,
+    ConfigModule.forRoot(),
+  ],
 })
 export class AppModule {}
