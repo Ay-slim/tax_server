30fe4c0c7176838976be2ec10568f5285ad8ada8
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-16 16:37:28 +0000

    Set headers and return 200 for OPTIONS request

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,21 @@ async function bootstrap() {
   app.enableCors({
     origin: ['http://localhost:3000', 'https://tax-ui-nu.vercel.app/'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
-    credentials: false,
+    credentials: true,
+  });
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization',
+    );
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    next();
   });
   await app.listen(3002);
 }
