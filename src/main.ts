70dec62a26c4846bfc79c0b0e2ea4313a25e0ce1
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-16 17:44:22 +0000

    Restore cors and fix error (shouldn't have added final slash to live UI domain)

--- src/main.ts
+++ src/main.ts
@@ -3,38 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.enableCors({
-  //   origin: ['http://localhost:3000', 'https://tax-ui-nu.vercel.app/'],
-  //   methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-  //   credentials: true,
-  // });
-  // app.use((req, res, next) => {
-  //   res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
-  //   res.setHeader(
-  //     'Access-Control-Allow-Methods',
-  //     'GET, POST, PATCH, DELETE, OPTIONS',
-  //   );
-  //   res.setHeader(
-  //     'Access-Control-Allow-Headers',
-  //     'Content-Type, Authorization',
-  //   );
-  //   res.setHeader('Access-Control-Allow-Credentials', 'true');
-
-  //   if (req.method === 'OPTIONS') {
-  //     console.log('Handling preflight request');
-  //     res.setHeader(
-  //       'Access-Control-Allow-Methods',
-  //       'GET, POST, PATCH, DELETE, OPTIONS',
-  //     );
-  //     res.setHeader(
-  //       'Access-Control-Allow-Headers',
-  //       'Content-Type, Authorization',
-  //     );
-  //     res.setHeader('Access-Control-Allow-Credentials', 'true');
-  //     return res.status(200).end();
-  //   }
-  //   next();
-  // });
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://tax-ui-nu.vercel.app'],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    credentials: false,
+  });
   await app.listen(3002);
 }
 bootstrap();
