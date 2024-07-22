28667cea98df09b3bc2057655f2754b3a3f11ef7
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-22 21:44:17 +0000

    Add contributions and category to filings schema

--- src/database/schemas/filing.ts
+++ src/database/schemas/filing.ts
@@ -4,8 +4,9 @@ export const Filing = new mongoose.Schema(
   {
     user_id: mongoose.Types.ObjectId,
     description: String,
+    category: String,
     income: Number,
-    year: Number,
+    contributions: [String],
     date: Date,
     country_id: mongoose.Types.ObjectId,
     tax: Number,
