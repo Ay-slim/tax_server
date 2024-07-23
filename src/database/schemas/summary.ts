2a7eb205f68ccaddd40bfe57cd2375a58a96f5d0
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-23 22:40:12 +0000

    Add total income, taxes, and deductions to summary schema type

--- src/database/schemas/summary.ts
+++ src/database/schemas/summary.ts
@@ -5,10 +5,25 @@ export const Summary = new mongoose.Schema(
     user_id: mongoose.Types.ObjectId,
     year: Number,
     country_id: mongoose.Types.ObjectId,
+    total_income: Number,
     total_taxed_income: Number,
     total_deducted_tax: Number,
     current_tax_index: Number,
     current_tax_bracket: String,
+    taxes: [
+      {
+        amount: Number,
+        reason: String,
+        description: String,
+      },
+    ],
+    deductions: [
+      {
+        amount: Number,
+        reason: String,
+        description: String,
+      },
+    ],
   },
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
 );
