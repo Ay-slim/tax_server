dde6f937da52ff5df39afa5dac12882092a4bbfc
Ay-slim <ayooluwaadedipe@gmail.com>
2024-07-23 22:40:38 +0000

    Make amount and category updates to filing schema

--- src/database/schemas/filing.ts
+++ src/database/schemas/filing.ts
@@ -4,12 +4,15 @@ export const Filing = new mongoose.Schema(
   {
     user_id: mongoose.Types.ObjectId,
     description: String,
-    category: String,
-    income: Number,
+    category: {
+      type: String,
+      enum: ['regular_income', 'capital_gain', 'investment_income'],
+      default: 'regular_income',
+    },
+    amount: Number,
     contributions: [String],
     date: Date,
     country_id: mongoose.Types.ObjectId,
-    tax: Number,
   },
   { timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' } },
 );
